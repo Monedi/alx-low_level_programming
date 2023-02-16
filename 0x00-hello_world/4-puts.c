077de47718d2d5202b20988d4c9b96f7cb7ee7ec
Monedi <drtnedi@gmail.com>
2023-02-16 09:22:08 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* main - A program that prints a line with puts function
-* Return: 0 (success)
-*/
-nt main(void)
+ * main - A program that prints a line with puts function
+ * Return: 0 (success)
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
