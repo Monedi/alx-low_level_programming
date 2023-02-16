425ef22c94efd9d6c99b8e3d8a67ed78c4bf882f
Monedi <drtnedi@gmail.com>
2023-02-16 13:14:16 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
-#include <stdio.h.>
+#include <stdio.h>
 /**
  * main-A program that print a line using the printf function
+ *
  * Return: 0(success)
+ *
  */
 int main(void)
 
 {
 	printf("with  proper grammar, but the outcome is a piece of art,\n");
-return(0);
+	return(0);
 
 }
