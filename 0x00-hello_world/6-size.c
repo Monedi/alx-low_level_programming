3187f8229458b39579006ffcb213160b25faad71
Monedi <drtnedi@gmail.com>
2023-02-16 16:54:51 -0800

    100-intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/** 
+ * main -A program that prints the size of various  computer types 
+ */
+int main(void)
+
+{
+
+int main(void)
+
+}
+
+    char a;
+    int b;
+    long int c;
+    long long int d;
+    float f;
