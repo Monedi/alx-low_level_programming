9a10e8783a19d50ac35b1dfd83e0bb15705ceaec
Monedi <drtnedi@gmail.com>
2023-02-23 11:23:52 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,24 +1,20 @@
 #include "main.h"
 
 /**
- * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
+ * print_line - print n underscore characters
+ * @n: number of times to print _
  */
 void print_line(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-} else
-{
-int i;
+	int i;
 
-for (i = 1; i <= n; i++)
-{
-_putchar('_');
-}
-_putchar('\n');
-}
-
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
 }
 
